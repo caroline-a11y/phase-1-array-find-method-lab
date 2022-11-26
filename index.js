e12634617bdf787a320a7203dada841882bafad1
Caroline <caroline.syokau@student.moringaschool.com>
2022-11-26 21:57:49 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(record){
+    let game = record.find((game)=>{
+    if(game.result==='W'){
+        return true;
+    }
+  })
+      if(game){
+        return game.year
+      }
+}
